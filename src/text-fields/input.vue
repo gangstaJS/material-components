9ce02881e0079b1dda81c0f77cbf77fca66dae0c
gangstaJS <oz@ex.ua>
2017-01-13 17:20:25 +0200

    Fix inputs

--- src/text-fields/input.vue
+++ src/text-fields/input.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="mdl-textfield mdl-js-textfield">
-        <input class="mdl-textfield__input" type="text" id="sample1">
-        <label class="mdl-textfield__label" for="sample1">Text...</label>
+    <div class="mdl-textfield mdl-js-textfield" :class="className">
+        <input class="mdl-textfield__input" :type="type" id="sample1" :value="value" :pattern="type == 'number' ? '-?[0-9]*(\.[0-9]+)?':''">
+        <label class="mdl-textfield__label" for="sample1">{{label}}</label>
+        <span class="mdl-textfield__error" v-show="type == 'number'">Input is not a number!</span>
     </div>
 </template>
 
@@ -9,9 +10,30 @@
 import common from '../mixins/text-field';
 
 export default {
+  props: {
+    value: {
+        default: '',
+        required: false
+    },
+    type: {
+        type: String,
+        default: 'text',
+        required: false
+    },
+    label: {
+        type: String,
+        default: 'Text...',
+        required: false
+    },
+    className: {
+        type: Object,
+        required: false
+    }
+  },
   mounted () {
     componentHandler.upgradeElements(this.$el)
   },
+
   mixins: [common]
 }
 </script>

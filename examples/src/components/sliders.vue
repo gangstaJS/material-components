52ca49d09474419d16be0d96f50ad7ab57d4a62e
gangstaJS <oz@ex.ua>
2017-01-27 13:08:59 +0200

    Added sliders example code

--- examples/src/components/sliders.vue
+++ examples/src/components/sliders.vue
@@ -4,18 +4,30 @@
         <br>
         <span>Step 10</span>
         <m-slider v-model="amount" min="0" max="100" step="10" disabled></m-slider>
-        <hr>
 
         <p>
             {{amount}}
         </p>
+
+        <hr>
+
+        <div>
+            <div>Code:</div>
+            <textarea cols="100" rows="6" :value="examples"></textarea>
+        </div>
+
+
     </div>
 </template>
 <script>
     export default{
         data(){
             return{
-                amount: 0
+                amount: 0,
+                examples: `
+                    <m-slider v-model="amount" min="0" max="100"></m-slider>
+                    <m-slider v-model="amount" min="0" max="100" step="10" disabled></m-slider>
+                `
             }
         }
     }

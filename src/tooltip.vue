ab31bb0c38717c6fac8b59ff021bf65f15510c34
gangstaJS <oz@ex.ua>
2017-02-09 19:30:35 +0200

    Updated packages

---
+++ src/tooltip.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header-component/>
+        <div>this is template body</div>
+        <other-component/>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return {}
+        }
+    }
+</script>
